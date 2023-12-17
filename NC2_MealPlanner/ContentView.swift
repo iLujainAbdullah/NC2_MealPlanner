cd47c9ab9a43dc207678f69fff8aeb36133c5189
iLujainAbdullah <lujain.abh@gmail.com>
2023-12-17 15:53:28 +0300

    Initial Commit

---
+++ NC2_MealPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC2_MealPlanner
+//
+//  Created by Lujain Abdullah Halabi Almeri on 04/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC2_MealPlanner/NC2_MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_MealPlannerApp.swift
+//  NC2_MealPlanner
+//
+//  Created by Lujain Abdullah Halabi Almeri on 04/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct NC2_MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
